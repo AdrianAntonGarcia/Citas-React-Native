8bb5e9abc0e0d34571187e57b0e574a76f1a1fb0
adriton <adrtler@gmail.com>
2021-01-30 17:29:02 +0100

    Botón para eliminar citas

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, TouchableHighlight} from 'react-native';
 
 const Cita = ({cita}) => {
+  const dialogoEliminar = () => {
+    console.log('Eliminando');
+  };
   return (
-    <View>
+    <View style={styles.cita}>
       <View>
-        <Text>Paciente</Text>
-        <Text>{cita.paciente}</Text>
+        <Text style={styles.label}>Paciente</Text>
+        <Text style={styles.texto}>{cita.paciente}</Text>
       </View>
       <View>
-        <Text>Propietario</Text>
-        <Text>{cita.propietario}</Text>
+        <Text style={styles.label}>Propietario</Text>
+        <Text style={styles.texto}>{cita.propietario}</Text>
       </View>
       <View>
-        <Text>Sintomas</Text>
-        <Text>{cita.sintomas}</Text>
+        <Text style={styles.label}>Sintomas</Text>
+        <Text style={styles.texto}>{cita.sintomas}</Text>
+      </View>
+      <View>
+        <TouchableHighlight
+          onPress={() => dialogoEliminar()}
+          style={styles.btnElminar}>
+          <Text style={styles.textoEliminar}> Eliminar &times;</Text>
+        </TouchableHighlight>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cita: {
+    backgroundColor: '#FFF',
+    borderBottomColor: '#e1e1e1',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    // paddingVertical: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingHorizontal: 10,
+    // paddingLeft: 10,
+    // paddingRight: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 20,
+  },
+  texto: {
+    fontSize: 14,
+  },
+  btnElminar: {
+    padding: 10,
+    backgroundColor: 'red',
+    marginVertical: 10,
+  },
+  textoEliminar: {
+    color: '#FFF',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default Cita;
